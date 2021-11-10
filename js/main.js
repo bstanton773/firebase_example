eb7af58499a054fb019c1f4d3acd2f34ee5e1ce2
Brian <bstanton773@gmail.com>
2021-11-10 10:29:53 -0600

    First commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+console.log('Hello we are connected')
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDKhOCmDUa1llDigMm-lEL9xt_1YeD_HcI",
+    authDomain: "kekambas-72-bs.firebaseapp.com",
+    projectId: "kekambas-72-bs",
+    storageBucket: "kekambas-72-bs.appspot.com",
+    messagingSenderId: "134801156454",
+    appId: "1:134801156454:web:6cb3d5efdd5cb5a17a00a7"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
+console.log(firebase);
